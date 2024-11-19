ee4f62663889ae0c1427d500d7a15ad32bb60f4b
gsvmbox <140973095+gsvmbox@users.noreply.github.com>
2024-11-19 11:55:34 +0300

    19112024

---
+++ Behavioral/Iterator/IteratorProducts/IteratorProducts.cpp
@@ -0,0 +1,149 @@
+﻿#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+//Добавление карточек товаров и работа с ними при помощи итераторов
+// 
+//класс товаров Products
+class Product {
+private:
+    string product_name;
+    int quantity;
+public:
+    //конструктор инициализации товаров
+    Product(string _product_name, int _quantity) {
+        product_name = _product_name;
+        quantity = _quantity;
+    }
+
+    //Вывод информации о товарах
+    string toString() {
+        return "Наименование товара: " + product_name + ", Количество: " + to_string(quantity);
+    }
+};
+
+//Контейнер для хрангения карточек товаров и работы с ними при помощи итераторов
+template <typename Container>
+class Iterator {
+private:
+    //Указатель на объект карточки товара
+    Container* containerData;
+
+    //Объявление итератора
+    list<Product>::iterator iter;
+
+public:
+    //Конструктор для инициализации итератора устанавливает указатель на первый элемент контейнера
+    Iterator(Container* _containerData) {
+        containerData = _containerData;
+
+        iter = containerData->cartList.begin();
+    }
+
+    //Проверка наличия ссылки на следующий элемент коллекции
+    bool hasNext() {
+        return (iter != containerData->cartList.end());
+    }
+
+    //Переключенеи итератора на следующий элемент, возврат текущего элемента
+    list<Product>::iterator next() {
+        return iter++;
+    }
+};
+
+template <typename Container>
+class Iterator_2 {
+private:
+    //Указатель на объект карточки товара
+    Container* containerData;
+
+    //Объявление итератора
+    list<Product>::iterator iter;
+
+public:
+    //Конструктор для инициализации итератора устанавливает указатель на первый элемент контейнера
+    Iterator_2(Container* _containerData) {
+        containerData = _containerData;
+
+        iter = containerData->cartList.begin();
+    }
+
+    //Проверка наличия ссылки на следующий элемент коллекции
+    bool hasNext() {
+        return (iter != containerData->cartList.end());
+    }
+
+    //Переключенеи итератора на следующий элемент, возврат текущего элемента
+    list<Product>::iterator next() {
+        iter++;
+        return iter++;
+    }
+};
+
+class Cart {
+private:
+    //Вспомогательный контейнер для хранения карточек товаров
+    list<Product> cartList;
+
+public:
+    //Метод добавления товара (в список)
+    void addProduct(Product a) {
+        cartList.push_back(a);
+    }
+
+    //Создание итератора
+    Iterator<Cart>* createIterator() {
+        return new Iterator<Cart>(this);
+    }
+
+    Iterator_2<Cart>* createIterator_2() {
+        return new Iterator_2<Cart>(this);
+    }
+
+    //Чтобы иметь возможность исполдьзовать объекты классов Iterator
+    friend class Iterator<Cart>;
+    friend class Iterator_2<Cart>;
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    //Создание карточек
+   Cart cartProducts;
+
+    //Создание товаров
+    Product a("Мышь", 4), b("Монитор", 5), c("SSD", 8), d("Клавиатура", 2);
+
+    //Добавление товаров
+    cartProducts.addProduct(a);
+    cartProducts.addProduct(b);
+    cartProducts.addProduct(c);
+    cartProducts.addProduct(d);
+
+    //Создание итератора для карточек
+    Iterator<Cart>* it = cartProducts.createIterator();
+    Iterator_2<Cart>* it_2 = cartProducts.createIterator_2();
+
+    //Работа с коллекцией
+    cout << "***Базовый Итератор***" << endl;
+    while (it->hasNext()) {
+        cout << it->next()->toString() << endl;
+    }
+    //Работа с альтернативным итератором
+    cout <<endl<< "***Итератор для четных позиций товара***" << endl;
+    while (it_2->hasNext()) {
+        cout << it_2->next()->toString() << endl;
+    }
+    //Работа со встроенным итератором контейнера list
+    cout << endl << "***Итератор контейнера list***" << endl;
+    list<Product> cards;
+    cards.push_back(a);
+    cards.push_back(b);
+    cards.push_back(c); 
+    cards.push_back(d);
+    for (auto iter = cards.begin(); iter != cards.end(); iter++)
+    {
+        cout << iter->toString() << endl;
+    }
+
+    return 0;
+}
---
+++ Behavioral/Mediator/MediatorCommon/MediatorCommon.cpp
@@ -0,0 +1,119 @@
+﻿#include <iostream>
+#include <string>
+#include <typeinfo> //для получения информации о типах RTTI 
+using namespace std;
+/**
+ * Интерфейс Посредника предоставляет метод, используемый компонентами для
+ * уведомления посредника о различных событиях. Посредник может реагировать на
+ * эти события и передавать исполнение другим компонентам.
+ */
+class BaseComponent;
+class Mediator {
+public:
+    virtual void Notify(BaseComponent* sender,string event) const = 0;
+};
+
+/**
+ * Базовый Компонент обеспечивает базовую функциональность хранения экземпляра
+ * посредника внутри объектов компонентов.
+ */
+class BaseComponent {
+protected:
+    Mediator* mediator_;
+
+public:
+    BaseComponent(Mediator* mediator = nullptr) : mediator_(mediator) {
+    }
+    void set_mediator(Mediator* mediator) {
+        this->mediator_ = mediator;
+    }
+    virtual void virtFunc() {}  //для нужд dynamic_cast
+
+};
+
+/**
+ * Конкретные Компоненты реализуют различную функциональность. Они не зависят от
+ * других компонентов. Они также не зависят от каких-либо конкретных классов
+ * посредников.
+ */
+class Component1 : public BaseComponent {
+public:
+    void DoA() {
+       cout << "Компонент 1 выполняет A.\n";
+        this->mediator_->Notify(this, "A");
+    }
+    void DoB() {
+       cout << "Компонент 1 выполняет B.\n";
+        this->mediator_->Notify(this, "B");
+    }
+};
+
+class Component2 : public BaseComponent {
+public:
+    void DoC() {
+       cout << "Компонент 2 выполняет C.\n";
+        this->mediator_->Notify(this, "C");
+    }
+    void DoD() {
+       cout << "Компонент 2 выполняет D.\n";
+        this->mediator_->Notify(this, "D");
+    }
+};
+
+/**
+ * Конкретные Посредники реализуют совместное поведение, координируя отдельные
+ * компоненты.
+ */
+class ConcreteMediator : public Mediator {
+private:
+    Component1* component1_;
+    Component2* component2_;
+   
+
+public:
+    ConcreteMediator(Component1* c1, Component2* c2) : component1_(c1), component2_(c2) {
+        this->component1_->set_mediator(this);
+        this->component2_->set_mediator(this); 
+    }
+    void Notify(BaseComponent* sender,string event) const override {
+           
+        if (event == "A") {
+           cout << "Посредник реагирует на A и запускает следующие операции:\n";
+           this->component2_->DoC();
+           dynamic_cast<Component1*>(sender)->DoB(); //или так, используя нисхоящее приведение типов
+           cout << typeid(*sender).name() << endl;
+        }
+        if (event == "D") {
+           cout << "Посредник реагирует на D и запускает следующие операции:\n";
+            this->component1_->DoB();
+            this->component2_->DoC();
+            dynamic_cast<Component2*>(sender)->DoC(); //или так, используя нисхоящее приведение типов 
+            cout << typeid(*sender).name() << endl;
+        }
+    }
+};
+
+/**
+ * Клиентский код.
+ */
+
+void ClientCode() {
+    Component1* c1 = new Component1;
+    Component2* c2 = new Component2;
+    ConcreteMediator* mediator = new ConcreteMediator(c1, c2);
+   cout << "Клиент запускает операцию A.\n";
+    c1->DoA();
+   cout << "\n";
+   cout << "Клиент запускает операцию D.\n";
+    c2->DoD();
+
+    delete c1;
+    delete c2;
+    delete mediator;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    ClientCode();
+    return 0;
+}
